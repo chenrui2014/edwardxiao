2f3f54d55a8a4d13a67a1827827ea5f0d85d28cd
expade <edwardfhsiao@gmail.com>
2017-02-21 22:15:01 +0800

    no message

--- app/frontend/webpack/base.js
+++ app/frontend/webpack/base.js
@@ -18,7 +18,11 @@ var config = module.exports = {
     index: PATH.ROOT_PATH + 'app/frontend/js/index.js',
   },
   module: {
-    {
+    preLoaders: [{
+      test: /\.js$/,
+      loader: "eslint-loader",
+      exclude: /node_modules/
+    }, {
       test: /\.css$/,
       loaders: ['postcss'],
       include: PATH.ROOT_PATH + 'frontend/css'
