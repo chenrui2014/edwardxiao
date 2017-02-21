11fd93df0ff6af32930644565e4c82bbd3f9d8cc
expade <edwardfhsiao@gmail.com>
2017-02-21 21:08:08 +0800

    Test

--- app/frontend/js/App/components/Modal/Index.js
+++ app/frontend/js/App/components/Modal/Index.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 
-import {
-  MODAL_CONTENT_COMPONENT_OBJECT,
-} from '../../reducers/ConstValue';
 
 class Modal extends Component {
 
@@ -13,45 +10,15 @@ class Modal extends Component {
   }
 
   render() {
-    let {
-      modalContentName,
-      className,
-    } = this.props;
-    let title;
-    let Component = MODAL_CONTENT_COMPONENT_OBJECT[modalContentName];
-    if (!className){
-      className = '';
-    }
+
     return(
       <div className={`modal fade ${className}`} id="myModal" tabIndex="-1" role="dialog" aria-hidden="true">
-        <div className="modal-dialog" role="document">
-          <Component/>
-        </div>
+        s
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  let {
-    modalContentName,
-  } = state;
-  return {
-    modalContentName,
-  };
-}
-function mapDispatchToProps(dispatch) {
-  return {
 
-  };
-}
-
-Modal.contextTypes = {
-  router: React.PropTypes.object
-};
-
-Modal.propTypes = {
-  modalContentName: React.PropTypes.string,
-}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Modal);
