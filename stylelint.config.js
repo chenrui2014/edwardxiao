db062c3eb29457c77c60606a803b9a6539fc3cfc
expade <edwardfhsiao@gmail.com>
2017-02-27 17:28:02 +0800

    Prepare webpack2

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,23 +5,20 @@ module.exports = {
     'color-hex-case': 'lower',
     'color-hex-length': null,
     'color-no-invalid-hex': true,
-    'length-zero-no-unit': true,
+    'length-zero-no-unit': null,
     'comment-empty-line-before': ['always', {
       'except': ['first-nested'],
       'ignore': ['stylelint-commands', 'between-comments'],
     }],
     'declaration-colon-space-after': 'always',
     'max-empty-lines': 2,
-    'rule-empty-line-before': ['always', {
-      'except': ['first-nested'],
-      'ignore': ['after-comment'],
-    }],
     'unit-whitelist': ['em', 'rem', '%', 's', 'ms', 'px', 'deg', 'vw', 'vh', 'dpi', 'dppx'],
     'selector-combinator-space-after': null,
     'selector-pseudo-element-colon-notation': null,
     'selector-list-comma-newline-after': null,
     'comment-empty-line-before': null,
     'block-closing-brace-newline-before': null,
-    'number-leading-zero': null
+    'number-leading-zero': null,
+    'rule-empty-line-before': null
   }
 };
